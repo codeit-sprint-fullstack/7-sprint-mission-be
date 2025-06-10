b9f24db98dda07246f430450ea6462b7aae1912e
dudska12 <skadud0113@naver.com>
2025-06-10 09:18:39 +0900

    feat:prisma db설정 및 api 구현

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const { name, description, price, tags } = req.body;
+    if (!name || !description || !price || !tags) {
+      return res.status(400).json({ error: "모든 필드에 입력해주세요." });
+    }
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price: parseInt(price),
+        tags,
+      },
+    });
+    return res.status(201).json(product);
+  } catch (error) {
+    console.log("post요청 실패", error);
+    return res.status(500).json({ error: "서버오류발생" });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!product) {
+      return res.status(404).json({ error: "해당 상품을 찾을 수 없습니다. " });
+    }
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: " 서버 오류 발생" });
+  }
+});
+
+app.patch("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const fields = ["name", "description", "price", "tags"];
+    const fieldsData = {};
+
+    for (const otherField of fields) {
+      if (req.body[otherField] !== undefined) {
+        fieldsData[otherField] =
+          otherField === "price"
+            ? parseInt(req.body[otherField])
+            : req.body[otherField];
+      }
+    }
+
+    if (Object.keys(filedsData).length == 0) {
+      return res.status(400).json({ error: "수정할 내용이 없습니다." });
+    }
+    const updatedProduct = await prisma.product.update({
+      where: { id },
+      data: fieldsData,
+    });
+
+    return res.status(200).json(updatedProduct);
+  } catch (error) {
+    return res.status(500).json({ error: "서버 오류 발생 " });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    return res.status(200).json({ message: "상품이 삭제되었습니다. " });
+  } catch (error) {
+    return res.status(500).json({ error: "서버 오류 발생" });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
