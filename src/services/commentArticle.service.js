5c60306e8ba80e8162a4a0dcd2c0273935c9a0d9
Woojin <jwj3171@gmail.com>
2025-07-28 11:48:04 +0900

    feat(commentArticle): 댓글 좋아요 토글 트랜잭션 적용

--- src/services/commentArticle.service.js
+++ src/services/commentArticle.service.js
@@ -10,6 +10,7 @@ import {
   deleteLike,
   countLikes,
 } from "../repositories/commentArticle.repository.js";
+import { toggleArticleCommentLikeWithTx } from "../utils/toggleArticleCommentLike.js";
 
 //댓글목록 + 유저별 liked 여부 계산
 export async function fetchCommentsByArticleId(articleId, userId) {
@@ -45,46 +46,7 @@ export async function deleteComment({ commentId, userId }) {
 }
 
 // 좋아요 토글
+
 export async function toggleArticleCommentLike({ commentId, userId }) {
-  const existing = await findLike({ commentId, userId });
-
-  if (existing) {
-    await deleteLike({ commentId, userId });
-    const likeCount = await countLikes(commentId);
-    return { liked: false, likeCount };
-  } else {
-    await createLike({ commentId, userId });
-    const likeCount = await countLikes(commentId);
-    return { liked: true, likeCount };
-  }
+  return await toggleArticleCommentLikeWithTx({ commentId, userId });
 }
-
-// export async function fetchCommentsByArticleId(articleId,userId) {
-//   return await findCommentsByArticleId(articleId,userId);
-// }
-
-// export async function createComment({ articleId, userId, content }) {
-//   return await insertComment({ articleId, userId, content });
-// }
-
-// export async function updateComment({ commentId, userId, content }) {
-//   return await modifyComment({ commentId, userId, content });
-// }
-
-// export async function deleteComment({ commentId, userId }) {
-//   return await softDeleteComment({ commentId, userId });
-// }
-
-// export async function toggleArticleCommentLike({ commentId, userId }) {
-//   const existingLike = await findCommentLike({ commentId, userId });
-
-//   if (existingLike) {
-//     await deleteCommentLike({ commentId, userId });
-//     const likeCount = await countCommentLikes(commentId);
-//     return { liked: false, likeCount };
-//   } else {
-//     await createCommentLike({ commentId, userId });
-//     const likeCount = await countCommentLikes(commentId);
-//     return { liked: true, likeCount };
-//   }
-// }
